17ec9d472be50c3948987bf357ce2435ba737990
sabkidukan <52843311+sabkidukan@users.noreply.github.com>
2019-07-13 06:51:07 +0530

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <body>
+    <div class="topnav">
+          <a class="active" href="#home">Home</a>
+          <a
+            href="https://www.itcportal.com/businesses/fmcg/foods/sunfeast.aspx">ITC
+            Food </a>
+          <a href="#Parle">Parle Products</a>
+          <a href="#Haldiram">Haldiram</a>
+          <a href="#Lakme">Lakme products</a>
+          <a href="#Gilette">Gilette products</a>
+          <a href="Eggs">Eggs & Bread</a>
+          <a href="DailyNeeds">DailyNeed Items</a>
+
+          <p>
+            <input type="text" id="bar" placeholder="Search Items"
+              onchange="search()"></p>
+        </div>
+
+
+
+        <div>
+          SHOPPING LIST
+
+        
+      </div>
+      <div class="row">
+        <link
+          href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css"
+          rel="stylesheet" id="bootstrap-css">
+        <script
+          src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <!------ Include the above in your HEAD tag ---------->
+
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 col-md-10 col-md-offset-1">
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th>Product</th>
+                    <th>Quantity</th>
+                    <th class="text-center">Price</th>
+                    <th class="text-center">Total</th>
+                    <th> </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="col-sm-8 col-md-6">
+                      <div class="media">
+                        <a class="thumbnail pull-left" href="#"> <img
+                            class="media-object"
+                            src="https://www.itcportal.com/businesses/fmcg/foods/images/yumitos-masala-large.jpg"
+                            style="width: 72px; height: 72px;"> </a>
+                        <div class="media-body">
+                          <h4 class="media-heading"><a href="#">Product name</a></h4>
+                          <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
+                          <span>Status: </span><span class="text-success"><strong>In
+                              Stock</strong></span>
+                        </div>
+                      </div></td>
+                    <td class="col-sm-1 col-md-1" style="text-align: center">
+                      <input type="email" class="form-control"
+                        id="exampleInputEmail1" value="3">
+                    </td>
+                    <td class="col-sm-1 col-md-1 text-center"><strong>$4.87</strong></td>
+                    <td class="col-sm-1 col-md-1 text-center"><strong>Rs.10.00</strong></td>
+                    <td class="col-sm-1 col-md-1">
+                      <button type="button" class="btn btn-danger">
+                        <span class="glyphicon glyphicon-remove"></span> Remove
+                      </button></td>
+                  </tr>
+                  <tr>
+                    <td class="col-md-8">
+                      <div class="media">
+                        <a class="thumbnail pull-left" href="#"> <img
+                            class="media-object"
+                            src="https://www.itcportal.com/businesses/fmcg/foods/images/Dark-Fantasy-Choco-Fills-big.jpg"
+                            style="width: 72px; height: 72px;"> </a>
+                        <div class="media-body">
+                          <h4 class="media-heading"><a href="#">Product name</a></h4>
+                          <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
+                          <span>Status: </span><span class="text-warning"><strong>Leaves
+                              warehouse in 1 - 2 weeks</strong></span>
+                        </div>
+                      </div></td>
+                    <td class="col-md-1" style="text-align: center">
+                      <input type="email" class="form-control"
+                        id="exampleInputEmail1" value="2">
+                    </td>
+                    <td class="col-md-1 text-center"><strong>Rs4.99</strong></td>
+                    <td class="col-md-1 text-center"><strong>Rs.9.98</strong></td>
+                    <td class="col-md-1">
+                      <button type="button" class="btn btn-danger">
+                        <span class="glyphicon glyphicon-remove"></span> Remove
+                      </button></td>
+                  </tr>
+                  <tr>
+                      <td class="col-md-3">
+                          <div class="media">
+                            <a class="thumbnail pull-left" href="#"> <img
+                                class="media-object"
+                                src="https://www.itcportal.com/businesses/fmcg/foods/images/yumitos-tomato.jpg"
+                                style="width: 72px; height: 72px;"> </a>
+                            <div class="media-body">
+                              <h4 class="media-heading"><a href="#">Product name</a></h4>
+                              <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
+                              <span>Status: </span><span class="text-warning"><strong>Leaves
+                                  warehouse in 1 - 2 weeks</strong></span>
+                            </div>
+                          </div></td>
+                        <td class="col-md-1" style="text-align: center">
+                          <input type="email" class="form-control"
+                            id="exampleInputEmail1" value="2">
+                        </td>
+                        <td class="col-md-1 text-center"><strong>Rs4.99</strong></td>
+                        <td class="col-md-1 text-center"><strong>Rs.9.98</strong></td>
+                        <td class="col-md-1">
+                          <button type="button" class="btn btn-danger">
+                            <span class="glyphicon glyphicon-remove"></span> Remove
+                          </button></td>
+                  </tr>
+                  <tr>
+                      <td class="col-md-4">
+                          <div class="media">
+                            <a class="thumbnail pull-left" href="#"> <img
+                                class="media-object"
+                                src="https://www.itcportal.com/businesses/fmcg/foods/images/as-you-like-it-large.jpg"
+                                style="width: 72px; height: 72px;"> </a>
+                            <div class="media-body">
+                              <h4 class="media-heading"><a href="#">Product name</a></h4>
+                              <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
+                              <span>Status: </span><span class="text-warning"><strong>Leaves
+                                  warehouse in 1 - 2 weeks</strong></span>
+                            </div>
+                          </div></td>
+                        <td class="col-md-1" style="text-align: center">
+                          <input type="email" class="form-control"
+                            id="exampleInputEmail1" value="2">
+                        </td>
+                        <td class="col-md-1 text-center"><strong>Rs4.99</strong></td>
+                        <td class="col-md-1 text-center"><strong>Rs.9.98</strong></td>
+                        <td class="col-md-1">
+                          <button type="button" class="btn btn-danger">
+                            <span class="glyphicon glyphicon-remove"></span> Remove
+                          </button></td>
+
+                  </tr>
+                  <tr>
+                      <td class="col-md-7">
+                          <div class="media">
+                            <a class="thumbnail pull-left" href="#"> <img
+                                class="media-object"
+                                src="https://www.itcportal.com/businesses/fmcg/images/agarbatti4.jpg"
+                                style="width: 72px; height: 72px;"> </a>
+                            <div class="media-body">
+                              <h4 class="media-heading"><a href="#">Product name</a></h4>
+                              <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
+                              <span>Status: </span><span class="text-warning"><strong>Leaves
+                                  warehouse in 1 - 2 weeks</strong></span>
+                            </div>
+                          </div></td>
+                        <td class="col-md-1" style="text-align: center">
+                          <input type="email" class="form-control"
+                            id="exampleInputEmail1" value="2">
+                        </td>
+                        <td class="col-md-1 text-center"><strong>Rs4.99</strong></td>
+                        <td class="col-md-1 text-center"><strong>Rs.9.98</strong></td>
+                        <td class="col-md-1">
+                          <button type="button" class="btn btn-danger">
+                            <span class="glyphicon glyphicon-remove"></span> Remove
+                          </button></td>
+
+
+                  </tr>
+                  <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td><h5>Subtotal</h5></td>
+                    <td class="text-right"><h5><strong>Rs.24.59</strong></h5></td>
+                  </tr>
+                  <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td><h5>Estimated shipping</h5></td>
+                    <td class="text-right"><h5><strong>RS.6.94</strong></h5></td>
+                  </tr>
+                  <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td><h3>Total</h3></td>
+                    <td class="text-right"><h3><strong>RS31.53</strong></h3></td>
+                  </tr>
+                  <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td>
+                      <button type="button" class="btn btn-default">
+                        <span class="glyphicon glyphicon-shopping-cart"></span>
+                        Continue Shopping
+                      </button></td>
+                    <td>
+                      <button type="button" class="btn btn-success">
+                        Checkout <span class="glyphicon glyphicon-play"></span>
+                      </button></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </body>
+
+
+
+</head>
+</html>
